300006db559b25ec6a62aa7e7ba3c20e014cadaa
David Sivocha <david@sivocha.com>
2016-09-19 23:38:03 +0100

    Add input masks

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
+$(function() {
+    var number        = $('#cc-number'),
+        expDate       = $('#cc-expiration-date'),
+        cvv           = $('#cc-cvv'),
+        paymentButton = $('#submit-payment');
 
+        //set the masks
+        number.inputmask("9999 9999 9999 9[999] [999]", {"placeholder": "4242 4242 4242 4242"});
+        expDate.inputmask("mm/yyyy", {"placeholder" : "01/2016"});
+        cvv.inputmask("999[9]", {"placeholder" : "123"});
+
+        number.focus();
 });
